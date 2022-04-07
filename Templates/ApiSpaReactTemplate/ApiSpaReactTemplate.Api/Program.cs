e167192ac1e43b794cf32623b4549740bad512e8
Bunyamin Sakar <bunyamin.sakar@live.nl>
2022-04-07 18:24:44 +0200

    added asp.net with spa react template

---
+++ Templates/ApiSpaReactTemplate/ApiSpaReactTemplate.Api/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
---
+++ Templates/ApiSpaReactTemplate/ApiSpaReactTemplate.Web/src/setupProxy.js
@@ -0,0 +1,15 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+    "/swagger",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7034',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
